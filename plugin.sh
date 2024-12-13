0a2d8ccb131321ca6cf4226ea5e761999d8265f5
NicholasIann <155471312+NicholasIann@users.noreply.github.com>
2024-12-13 11:47:06 +0100

    Update plugin.sh

--- plugin.sh
+++ plugin.sh
@@ -1,5 +1,16 @@
+#!/bin/bash
+
+function dynamic_network_adjuster_install {
+    echo "Installing Dynamic Network Adjuster Plugin..."
+    pip install psutil flask
+}
+
 function dynamic_network_adjuster_start {
-    python3 $DEST/plugins/dynamic_network_adjuster/dynamic_network_adjuster.py
+    # Avvia il monitoraggio della rete in background
+    python3 $DEST/plugins/dynamic_network_adjuster/dynamic_network_adjuster.py &
+
+    # Avvia la UI Flask
+    python3 $DEST/plugins/dynamic_network_adjuster/ui/dynamic_network_adjuster_ui.py &
 }
 
 if [[ "$1" == "stack" ]]; then
@@ -12,3 +23,4 @@ if [[ "$1" == "stack" ]]; then
             ;;
     esac
 fi
+
